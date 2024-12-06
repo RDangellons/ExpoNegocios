898c302ddc46871db538bd6f5275a5ba1d516778
Angel Alonso Rodriguez <angel2gol@hotmail.com>
2024-12-06 11:14:17 -0600

    comienzo del proyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+// Suavizar el scroll para los enlaces
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
